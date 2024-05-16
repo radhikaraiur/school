b6ce6d302ad43b15651c29fbcba573315bfc3635
akashrajaput <164490652+radhikaraiur@users.noreply.github.com>
2024-05-16 13:36:32 +0530

    Add files via upload

---
+++ 22.py
@@ -0,0 +1,9 @@
+a=int(input("84"))
+b=int(input("64"))
+c=int(input("76"))
+if(a>b and a>c):
+    print(f"{a}is bigger")
+   elif (b>a and b>c):
+       print(f"{b}is bigger")
+    else:
+        print(f"{c}is bigger")
